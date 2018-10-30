cbd944ea63c446dda7f4462a535fc7377a4ce347
SHARJEEL KHAN <sharjeelkhan760@gmail.com>
2018-10-30 05:15:05 -0700

    updated

--- src/gitapp/Gitapp.java
+++ src/gitapp/Gitapp.java
@@ -15,7 +15,8 @@ public class Gitapp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.print("sharjeel khan");
     }
     
 }
